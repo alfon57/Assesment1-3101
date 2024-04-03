c73a612c5ce83dfcfd7b5f01559b4bf36fb8a79a
Alfonsus Liguori Dango <alfonsusxiaomi876@gmail.com>
2024-04-03 14:40:30 +0700

    .Menambahkan Navigation component

--- app/src/main/java/org/d3if3101/assesment1/MainActivity.kt
+++ app/src/main/java/org/d3if3101/assesment1/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import org.d3if3101.assesment1.screen.MainScreen
+import org.d3if3101.assesment1.navigation.SetUpNavGraph
 import org.d3if3101.assesment1.ui.theme.Assesment1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    SetUpNavGraph()
                 }
             }
         }
---
+++ app/src/main/java/org/d3if3101/assesment1/navigation/NavGraph.kt
@@ -0,0 +1,24 @@
+package org.d3if3101.assesment1.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.d3if3101.assesment1.screen.MainScreen
+
+
+@Composable
+fun SetUpNavGraph(navController: NavHostController = rememberNavController())
+{
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    )
+    {
+        composable(route = Screen.Home.route)
+        {
+            MainScreen()
+        }
+    }
+}
