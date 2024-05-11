c3652dcbf1d14ed9ef78b9cccd2c11c8d4a8f431
Alfonsus Liguori Dango <alfonsusxiaomi876@gmail.com>
2024-05-11 10:16:38 +0700

    Membuat tampilan staggered grid

--- app/src/main/java/org/d3if3101/assesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3101/assesment1/ui/screen/MainScreen.kt
@@ -1,6 +1,7 @@
 package org.d3if3101.assesment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,8 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -31,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -93,12 +100,12 @@ fun MainScreen(navController: NavHostController)
             }
         }
     ) {padding ->
-        ScreenContent(Modifier.padding(padding), navController)
+        ScreenContent(showList,Modifier.padding(padding), navController)
     }
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier, navController: NavHostController)
+fun ScreenContent(showList: Boolean, modifier: Modifier, navController: NavHostController)
 {
     val context = LocalContext.current
     val db = ToDoDB.getInstance(context)
@@ -122,20 +129,37 @@ fun ScreenContent(modifier: Modifier, navController: NavHostController)
     }
     else
     {
-        LazyColumn (modifier = modifier
-            .fillMaxSize(),
-            contentPadding = PaddingValues(bottom = 84.dp)
-        )
-        {
-            items(data)
+        if (showList){
+            LazyColumn (modifier = modifier
+                .fillMaxSize(),
+                contentPadding = PaddingValues(bottom = 84.dp)
+            )
             {
-                ListItem(toDoList = it)
+                items(data)
                 {
-                    navController.navigate(Screen.FormUbah.withId(it.id))
+                    ListItem(toDoList = it)
+                    {
+                        navController.navigate(Screen.FormUbah.withId(it.id))
+                    }
+                    Divider()
                 }
-                Divider()
+            }
+        } else{
+            LazyVerticalStaggeredGrid(
+                modifier = modifier.fillMaxSize(),
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 8.dp,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 84.dp)
+            ){
+                items(data){
+                   GridItem(toDoList = it) {
+                       navController.navigate(Screen.FormUbah.withId(it.id))
+                   }
+                }
             }
         }
+
     }
 
 }
@@ -180,6 +204,56 @@ fun ListItem(toDoList: ToDoList,
             Text(text = toDoList.tanggal)
         }
 
+    }
+}
+
+@Composable
+fun GridItem(toDoList: ToDoList, onClick: () -> Unit){
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        border = BorderStroke(1.dp, Color.Gray)
+    ){
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        )
+        {
+            if(toDoList.status == "Selesai"){
+
+                Text(text = toDoList.judul,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(text = toDoList.isi,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.blur(3.dp)
+                )
+                Text(
+                    text = toDoList.tanggal,
+                    modifier = Modifier.blur(3.dp)
+                )
+            }else{
+                Text(text = toDoList.judul,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = toDoList.isi,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(text = toDoList.tanggal)
+            }
+
+        }
     }
 }
 
